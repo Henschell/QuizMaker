6ebf3103761e16b618b261b22da9bbc5324c0d09
Unknown <henschell_hans@hotmail.com>
2019-03-06 13:33:30 +0100

    1.2

---
+++ UnityQuizMaker/Assets/Scripts/ScriptQuizMakerScene/AnswerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class AnswerData : MonoBehaviour
+{
+
+    [Header("UI Elements")]
+    [SerializeField] TextMeshProUGUI infoTextObject;
+    [SerializeField] Image toggle;
+
+    [Header("textures")]
+    [SerializeField] Sprite uncheckedToggle;
+    [SerializeField] Sprite checkedToggle;
+
+    private int _answerIndex = -1;
+    public int AnswerIndex { get { return _answerIndex; } }
+
+}
---
+++ UnityQuizMaker/Assets/Scripts/ScriptQuizMakerScene/GameEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvents", menuName = "Quiz/new GameEvent")]
+public class GameEvents : ScriptableObject
+{
+
+    public delegate void UpdateQuestionUICallback(Question question);
+    public UpdateQuestionUICallback UpdateQuestionUI;
+
+    public delegate void UpdateQuestAnswerCallback(AnswerData pickedAnswer);
+    public UpdateQuestAnswerCallback UpdateQuestionAnswer;
+
+    public delegate void DisplayResolutionScreenCallback( int score); //UIManager.ResolutionScreenType type
+    public DisplayResolutionScreenCallback DisplayResolutionScreen;
+
+    public delegate void ScoreUpdatedCallback();
+    public ScoreUpdatedCallback ScoreUpdated;
+
+    public int CurrentFinalScore;
+
+
+
+}
