d29c1350bd6d2fd02ca764142683261bf28f8423
Marc <YouTube.Dragon836@gmx.de>
2019-03-06 13:29:38 +0100

    Added Timer

---
+++ UnityQuizMaker/Assets/ScriptFolder/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public int MyTimer = 120; // Wenn 0 erreicht wird, wird automatisch die nächste szene aufgerufen / Frage.
+    public Text MyTimerText;
+
+	void Start () {
+        StartCoroutine(TimeIt());
+    }
+	
+    IEnumerator TimeIt()
+    {       
+        MyTimer --;
+        yield return new WaitForSeconds(1);
+        MyTimerText.text = MyTimer.ToString();
+        StartCoroutine(TimeIt());
+    }
+}
