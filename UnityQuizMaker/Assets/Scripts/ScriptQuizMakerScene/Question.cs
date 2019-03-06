16e98938cb3e656bbc780c64b31c055f3cd3a4d8
Unknown <henschell_hans@hotmail.com>
2019-03-06 09:08:43 +0100

    Ver 1.1

---
+++ UnityQuizMaker/Assets/Scripts/ScriptQuizMakerScene/Question.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Question : MonoBehaviour {
+
+    [SerializeField] private string _info = string.Empty;
+    public string Info { get { return _info; } }
+}
